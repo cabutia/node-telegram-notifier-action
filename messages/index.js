a55a0b17ecee198e9931ff1c3fd1d10b7d3d5a24
Leadro Gomez <leandrogrotnig11@gmail.com>
2020-04-06 17:58:38 -0300

    Updates

--- messages/index.js
+++ messages/index.js
@@ -1,19 +1,19 @@
 module.exports.deployStarted = `
-Job _{{ job }}_ started!
+Job "{{ job }}" started!
 ------------------------------
 Project: {{ repository }}
 Commit: {{ commit }}
 `
 
 module.exports.deploySucceed = `
-Job _{{ job }}_ success!
+Job "{{ job }}" success!
 ------------------------------
 Project: {{ repository }}
 Commit: {{ commit }}
 `
 
 module.exports.deployFailed = `
-Job _{{ job }}_ failed!
+Job "{{ job }}" failed!
 ------------------------------
 Project: {{ repository }}
 Commit: {{ commit }}
